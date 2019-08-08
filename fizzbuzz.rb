df67be355573bcc0f8d9e6b925b7a0ee2beda332
Eric Taylor <eric@ericscotttaylor.com>
2019-08-08 04:40:21 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz
-  
+    if int % 3 && int % 5
+      put "FizzBuzz"
+    elsif int % 5
+      put "Buzz"
+    elsif int % 3
+      put "Fizz"
+    else
+      nil
 end
